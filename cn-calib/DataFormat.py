7deb0e8b166f8c72bd570660c8587c962913df33
niklai99 <lai.nicolo99@gmail.com>
2022-12-05 21:04:06 +0100

    adapting code to new setup

--- cn-calib/DataFormat.py
+++ cn-calib/DataFormat.py
@@ -1,34 +1,86 @@
 from dataclasses import dataclass
+import numpy as np
 
+class DataHistogram:
+    """Holds the structure of the detectors data"""
+    
+    def __init__(self, energy, time) -> None:
+        self.e_binceters  = np.array(0.5 * (energy[1][1:] + energy[1][:-1]))
+        self.e_binheights = energy[0]
+        self.t_binceters  = np.array(0.5 * (time[1][1:] + time[1][:-1]))
+        self.t_binhights  = time[0]
+        
+        
+    @property
+    def energy(self):
+        return self.e_binceters, self.e_binheights
+    
+    @property
+    def time(self):
+        return self.t_binceters, self.t_binhights
+    
+    
 
 @dataclass
-class DataFormat:
+class RootFileFormat:
     """Holds the structure of the .root file"""
     
-    root_tree : str = "Data_R"
+    # trees
+    energy_tree : str = "Energy"
+    time_tree   : str = "Time"
     
-    channel   : str = "Channel"
-    board     : str = "Board"
-    flag      : str = "Flags"
-    time      : str = "Timestamp"
-    energy    : str = "Energy"
+    # name prefix
+    energy_prefix : str = "_F_Energy"
+    time_prefix   : str = "_F_Time"
     
-    @property
-    def to_channel(self):
-        return f"{self.root_tree}/{self.channel}"
+    # boards
+    board_1     : str = "V1725C_446"
+    board_2     : str = "V1730B_281"
     
-    @property
-    def to_board(self):
-        return f"{self.root_tree}/{self.board}"
+    # active channels
+    a = f"CH2@{board_1}"
+    b = f"CH0@{board_2}"
+    c = f"CH1@{board_2}"
+    d = f"CH2@{board_2}"
+    e = f"CH3@{board_2}"
     
-    @property
-    def to_flag(self):
-        return f"{self.root_tree}/{self.flag}"
-    
-    @property
-    def to_time(self):
-        return f"{self.root_tree}/{self.time}"
     
     @property
-    def to_energy(self):
-        return f"{self.root_tree}/{self.energy}"
+    def to_a_energy(self):
+        return f"{self.energy_tree}/{self.energy_prefix}{self.a}"
+    
+    @property
+    def to_a_time(self):
+        return f"{self.time_tree}/{self.time_prefix}{self.a}"
+    
+    @property
+    def to_b_energy(self):
+        return f"{self.energy_tree}/{self.energy_prefix}{self.b}"
+    
+    @property
+    def to_b_time(self):
+        return f"{self.time_tree}/{self.time_prefix}{self.b}"
+    
+    @property
+    def to_c_energy(self):
+        return f"{self.energy_tree}/{self.energy_prefix}{self.c}"
+    
+    @property
+    def to_c_time(self):
+        return f"{self.time_tree}/{self.time_prefix}{self.c}"
+    
+    @property
+    def to_d_energy(self):
+        return f"{self.energy_tree}/{self.energy_prefix}{self.d}"
+    
+    @property
+    def to_d_time(self):
+        return f"{self.time_tree}/{self.time_prefix}{self.d}"
+    
+    @property
+    def to_e_energy(self):
+        return f"{self.energy_tree}/{self.energy_prefix}{self.e}"
+    
+    @property
+    def to_e_time(self):
+        return f"{self.time_tree}/{self.time_prefix}{self.e}"
--- cn-calib/read_data.py
+++ cn-calib/read_data.py
@@ -1,11 +1,17 @@
 import uproot
 import pandas as pd
 
-def read_data(data_path, file_name, data_format):
-    with uproot.open(f"{data_path}/{file_name}") as data_file:
-        channels = data_file[data_format.to_channel].array().to_numpy()
-        boards   = data_file[data_format.to_board].array().to_numpy()
-        flags    = data_file[data_format.to_flag].array().to_numpy()
-        time     = data_file[data_format.to_time].array().to_numpy()
-        energy   = data_file[data_format.to_energy].array().to_numpy()
-    return pd.DataFrame({"board": boards, "channel": channels, "flag": flags, "timestamp": time, "energy": energy})
+def read_data(file_name, data_format):
+    with uproot.open(file_name) as data_file:
+        a_energy = data_file[data_format.to_a_energy].to_numpy()
+        a_time   = data_file[data_format.to_a_time].to_numpy()
+        b_energy = data_file[data_format.to_b_energy].to_numpy()
+        b_time   = data_file[data_format.to_b_time].to_numpy()
+        c_energy = data_file[data_format.to_c_energy].to_numpy()
+        c_time   = data_file[data_format.to_c_time].to_numpy()
+        d_energy = data_file[data_format.to_d_energy].to_numpy()
+        d_time   = data_file[data_format.to_d_time].to_numpy()
+        e_energy = data_file[data_format.to_e_energy].to_numpy()
+        e_time   = data_file[data_format.to_e_time].to_numpy()
+        
+    return a_energy, a_time, b_energy, b_time, c_energy, c_time, d_energy, d_time, e_energy, e_time
