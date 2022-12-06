1e19b29fb76ef5e35e8972be7e738cc3fa1f1eb3
niklai99 <lai.nicolo99@gmail.com>
2022-12-06 18:55:51 +0100

    labr detectors calibrated

--- cn-calib/calib_fit.py
+++ cn-calib/calib_fit.py
@@ -4,14 +4,19 @@ from scipy.optimize import curve_fit
 def gaussian(x, a, b, c):
     return a * np.exp(-(x - b)**2 / (2 * c**2))
 
+def linear(x, a, b):
+    return a * x + b
+
 def fit_peak(func, x, y, err_y, e_fit, p0):
     par, cov = curve_fit(func, x[e_fit], y[e_fit], sigma=err_y[e_fit], p0=p0, absolute_sigma=True)
     err = np.sqrt(np.diag(cov))
     return par, np.abs(err)
 
 
-def build_parameter_string(par, err):
-    parameter_names = ["A", "$\mu$", "$\sigma$"]
+def build_parameter_string(par, err, parameter_names=None):
+    if parameter_names is None:
+        parameter_names = ["A", "$\mu$", "$\sigma$"]
+        
     par_str = ""
     for i in range(len(par)):
         par_str += f"{parameter_names[i]} = {par[i]:.3f} +/- {err[i]:.3f}"
@@ -25,3 +30,6 @@ def save_parameters(par, err, name):
     with open(f"./calib-parameters/{name}_parameters.txt", "w") as f:
         for i in range(len(par)):
             f.write(f"{parameter_names[i]} {par[i]} {err[i]}\n")
+            
+        
+    
